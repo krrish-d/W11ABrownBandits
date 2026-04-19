40c4897d6de1b931f36f05ae62b7216111848343
Venkat Srikrishna <z5593071@ad.unsw.edu.au>
2026-04-19 18:07:26 +1000

    Fixed .env issue not working with swagger

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from fastapi import FastAPI
 from app.routers import communicate, invoice, transform, validate
 from app.database import Base, engine
